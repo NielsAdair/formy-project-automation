c48a99f498f2d1691da02f68e9b7027fbb9be429
Niels Adair <Niels.Adair@gmail.com>
2023-03-12 10:27:07 -0600

    Initial commit

---
+++ Ex_Files_Selenium_EssT/Exercise Files/02_03/start/test/Autocomplete.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Autocomplete {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/nadai/Downloads/chromedriver_win32/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.sendKeys("1086 South 950 East, Provo, UT");
+        Thread.sleep( 1000);
+
+        WebElement autocompletResult = driver.findElement(By.className("pac-container"));
+
+        driver.quit();
+    }
+}
